001e6713266d3914b81fc4f5d732c169996aa931
Polar <92080533+KuppaHX@users.noreply.github.com>
2021-12-29 11:23:38 +0700

    Update FreeScript.lua

--- FreeScript.lua
+++ FreeScript.lua
@@ -1,7 +1,7 @@
 local PlaceId = game.PlaceId
 
 if PlaceId == 6284583030 then
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/KuppaHX/MOKURO-HUB/main/PetSimulatorX.lua'))()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/KuppaHX/Saza/main/PSX.lua'))()
 else
 	game.Players.LocalPlayer:kick("Wrong Game Onii chan><")
 	wait(1)
